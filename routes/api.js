04de1f13b1b68e036ad778fcdcb9f0fc6db0c6f9
E-K8 <ekatplat@gmail.com>
2024-06-01 22:03:43 +0100

    add MongoDB connection, define Issue schema

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,47 @@
 'use strict';
 
-module.exports = function (app) {
+const expect = require('chai').expect;
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+const uri =
+  'mongodb+srv://user_2023:' +
+  process.env.PW +
+  '@e-k8.30bqhdu.mongodb.net/issue_tracker?retryWrites=true&w=majority&appName=E-K8';
+
+module.exports = function (app) {
+  mongoose.connect(uri);
+
+  const issueSchema = new mongoose.Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    status_text: String,
+    open: { type: Boolean, required: true },
+    created_on: { type: Date, required: true },
+    updated_on: { type: Date, required: true },
+    project: String,
+  });
+
+  const URLModel = mongoose.model('Issue', issueSchema);
+
+  app
+    .route('/api/issues/:project')
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
