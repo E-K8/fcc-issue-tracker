c9aa7af745fd3e255e738bc6110a9a711561340a
E-K8 <ekatplat@gmail.com>
2024-06-04 19:50:05 +0100

    add post request to create documents

--- routes/api.js
+++ routes/api.js
@@ -24,7 +24,7 @@ module.exports = function (app) {
     project: String,
   });
 
-  const URLModel = mongoose.model('Issue', issueSchema);
+  const Issue = mongoose.model('Issue', issueSchema);
 
   app
     .route('/api/issues/:project')
@@ -35,6 +35,24 @@ module.exports = function (app) {
 
     .post(function (req, res) {
       let project = req.params.project;
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project,
+      });
+
+      newIssue.save((error, savedIssue) => {
+        if (!error && savedIssue) {
+          console.log('Saved issue is');
+          console.log(savedIssue);
+        }
+      });
     })
 
     .put(function (req, res) {
